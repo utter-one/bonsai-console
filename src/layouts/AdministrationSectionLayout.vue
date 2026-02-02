be1fc70045601a6c2e3a94f38fea87eea72c39e3
Patryk Bajer <patryk.bajer@gmail.com>
2026-02-02 22:11:58 +0100

    feat: update icons in AdministrationSectionLayout and enhance DashboardView with additional stats (#27)

--- src/layouts/AdministrationSectionLayout.vue
+++ src/layouts/AdministrationSectionLayout.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 import SectionLayout from './SectionLayout.vue'
-import { BriefcaseBusiness, User, Settings, Globe } from 'lucide-vue-next'
+import { BriefcaseBusiness, User, CloudCog, Globe } from 'lucide-vue-next'
 
 const menuItems = [
   { name: 'administration.projects', label: 'Projects', icon: BriefcaseBusiness },
   { name: 'administration.admins', label: 'Admins', icon: User },
-  { name: 'administration.providers', label: 'Providers', icon: Settings },
+  { name: 'administration.providers', label: 'Providers', icon: CloudCog },
   { name: 'administration.environments', label: 'Environments', icon: Globe },
 ]
 </script>
